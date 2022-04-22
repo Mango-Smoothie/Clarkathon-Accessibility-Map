d82bb4ecfd72b67b1d0fc75a2e8d1a72552bc818
Nathan Tran <natran411@gmail.com>
2022-04-22 01:31:00 -0400

    finalized

--- public/js/index.js
+++ public/js/index.js
@@ -20,14 +20,20 @@ $.getJSON("data/buildings.json", () => {
         console.log(current_id)
         building_data.forEach((b) => {
             if (b.id === current_id) {
+                console.log(b)
+                $("#infoLabel").empty()
+                $("#time_list").empty()
                 $("#infoLabel").text(b.name);
-                $(".modal-body").text(b.description);
+                b.description.forEach((d)=>{
+                    console.log(d)
+                    $("#time_list").append(getDescriptionBlock(d));
+                })
+                $("#elevator").text("Elevator: " + b.elevators)
             } else {
             }
         })
     })
 })
-
 
 function addNewFeedback() {
     location.href = "feedback.html"
@@ -37,13 +43,27 @@ function get_building_button(building, idx) {
     return `<button type="button" data-b="${building.id}" class="btn btn-building w-100 btn-block ${idx % 2 === 0 ? 'btn-danger' : 'btn-light'} " data-bs-toggle="modal" data-bs-target="#exampleModal">${building.id}) ${building.name}</button>`
 }
 
+function getDescriptionBlock(dsc_obj) {
+    return `<li class="list-group-item">
+                <div class="row ">
+                    <p>${dsc_obj}</p>
+                </div>
+          </li>`
+}
 
 function fillModal(id) {
     let current_id = id.toString()
     building_data.forEach((b) => {
         if (b.id === current_id) {
+            console.log(b)
+            $("#infoLabel").empty()
+            $("#time_list").empty()
             $("#infoLabel").text(b.name);
-            $(".modal-body").text(b.description);
+            b.description.forEach((d)=>{
+                console.log(d)
+                $("#time_list").append(getDescriptionBlock(d));
+            })
+            $("#elevator").text("Elevator: " + b.elevators)
         } else {
         }
     })
